f8d8328103958a8cb2b488810b59cc5141e0f62a
Lstei <sonyavolk69@gmail.com>
2023-11-14 02:49:18 +0600

    converting files

--- app.js
+++ app.js
@@ -1,50 +1,73 @@
 const express = require('express');
 const multer = require('multer');
+const urllib = require('urllib');
+const fs = require('fs');
+const { XMLParser } = require("fast-xml-parser");
+const jwt = require('jsonwebtoken');
+const secret = 'mJI8cO6yClKDyrSc8dFJbz4TP7qql3E5';
 const app = express();
 const port = 7000;
 
-
-const storageConfig = multer.diskStorage({
-	destination: function (req, file, cb) {
-		cb(null, 'uploads/')
-	},
-	filename: function (req, file, cb) {
-		cb(null, file.fieldname + '-' + Date.now())
-	}
-})
-
-//filtering uploaded files
-const fileFilter = (req, file, cb) => {
-
-	if(file.mimetype === "image.png" ||
-		file.mimetype === "image.jpg" ||
-		file.mimetype === "image.jpeg"
-		) {
-		cb(null, true);
-	}
-	else {
-		cb(null, false);
-	}
-}
-
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 app.use(express.static(__dirname));
-app.use(multer({dest:'uploads'}).single('filedata'));
-app.use(multer({storage:storageConfig, fileFilter: fileFilter}).single('filedata'));
-app.use(express.json({limir: '10mb'}));
-app.use(express.urlencoded({limit: '10mb'}));
+const upload = multer({ dest: 'uploads' });
 
-app.get('/', (req, res) => {
+app.get('/', (_, res) => {
 	res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(port, () => {
-	console.log(`Server is running on http://localhost:${port}`);
+app.post('/upload', upload.single('filedata'), async function (req, res) {
+	console.log(req.file);
+
+	//if (!req || !req.file) return
+
+	const postData = {
+		async: 'false',
+		title: req.file.originalname,
+		url: `http://localhost:7000/${req.file.path}`,
+		outputtype: 'pdf',
+		filetype: 'docx',
+		codePage: '65001',
+		key: makeId(14),
+	};
+
+	const token = jwt.sign(postData, secret, { algorithm: 'HS256', expiresIn: '5m' });
+
+	const options = {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			'Authorization': `Bearer ${token}`,
+		},
+		data: postData
+	};
+
+	const { status, data } = await urllib.request('http://localhost:80/ConvertService.ashx', options);
+
+	//if (status != 200) return
+
+	const xmlParser = new XMLParser();
+	const responseObj = xmlParser.parse(data.toString());
+	const responseFileUrl = responseObj.FileResult.FileUrl;
+
+	fs.unlink(req.file.path, _ => {});
+
+	res.redirect(responseFileUrl);
 });
 
-app.post('/upload', function (req, res) {
-	let filedata = req.file;
-	if(!filedata) {
-		res.send('Loading error')
-	}
-	res.send('File uploaded');
+function makeId(length) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      counter += 1;
+    }
+    return result;
+}
+
+app.listen(port, () => {
+	console.log(`Server is running on http://localhost:${port}`);
 });
